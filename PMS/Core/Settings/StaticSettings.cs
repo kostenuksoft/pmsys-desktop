8e5e34ba1c548d7b1f480549a02d5c2f7762a4b6
Vladyslav Kostenuk <kostenuksoft@outlook.jp>
2025-12-25 14:13:22 +0200

    Add application static settings.

---
+++ PMS/Core/Settings/StaticSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace PMS.Core.Settings;
+
+public static class SApplicationSettings
+{
+    public static string ApplicationName { get; set; } = "PMSys";
+    public static string Version { get; set; } = "1.00a";
+}
